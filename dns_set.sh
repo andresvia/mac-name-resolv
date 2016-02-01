a4473861441e903ae7245aae4ecc7f9d1b97e258
andres <andres.via@gmail.com>
2016-02-01 07:39:53 -0800

    selective dns_set.sh

--- dns_set.sh
+++ dns_set.sh
@@ -3,46 +3,60 @@
 docker_machine_name=${DEFAULT_DOCKER_MACHINE_NAME:-boot2docker}
 
 dns_set="${1:-empty}"
+what="${2:-network_service_dns|dlite|docker-machine|networksetup}"
+what="^(${what})\$"
 
 if [ "$dns_set"  != "empty" ]
 then
-  scutil <<< "list State:/Network/Service/[^/]+/DNS" |
-  while read subkey index eq network_service_dns
-  do
-    sudo scutil << EOF
-      open
-      d.init
-      d.add ServerAddresses * $dns_set
-      set $network_service_dns
-      quit
-EOF
-  done
-
-  if which dlite > /dev/null 2>&1
+  if [[ 'network_service_dns' =~ $what ]]
   then
-    ssh -T docker@local.docker << EOF > /dev/null
-    sudo tee /etc/resolv.conf << TEE
-    nameserver $dns_set
-TEE
+    scutil <<< "list State:/Network/Service/[^/]+/DNS" |
+    while read subkey index eq network_service_dns
+    do
+      sudo scutil << EOF
+        open
+        d.init
+        d.add ServerAddresses * $dns_set
+        set $network_service_dns
+        quit
 EOF
+    done
   fi
 
-  if which docker-machine > /dev/null 2>&1
+  if [[ 'dlite' =~ $what ]]
   then
-    if docker-machine ls -filter state=running | grep -q "^$docker_machine_name"
+    if which dlite > /dev/null 2>&1
     then
-      docker-machine ssh $docker_machine_name << EOF > /dev/null
-        sudo tee /etc/resolv.conf << TEE
-          nameserver $dns_set
+      ssh -T docker@local.docker << EOF > /dev/null
+      sudo tee /etc/resolv.conf << TEE
+      nameserver $dns_set
 TEE
 EOF
     fi
   fi
+
+  if [[ 'docker-machine' =~ $what ]]
+  then
+    if which docker-machine > /dev/null 2>&1
+    then
+      if docker-machine ls -filter state=running | grep -q "^$docker_machine_name"
+      then
+        docker-machine ssh $docker_machine_name << EOF > /dev/null
+          sudo tee /etc/resolv.conf << TEE
+            nameserver $dns_set
+TEE
+EOF
+      fi
+    fi
+  fi
 fi
 
-sudo networksetup -listallnetworkservices |
-sed 1d |
-while read iface
-do
-  sudo networksetup -setdnsservers "$iface" "$dns_set"
-done
+if [[ 'networksetup' =~ $what ]]
+then
+  sudo networksetup -listallnetworkservices |
+  sed 1d |
+  while read iface
+  do
+    sudo networksetup -setdnsservers "$iface" "$dns_set"
+  done
+fi
